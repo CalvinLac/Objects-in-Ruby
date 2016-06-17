ef5f5a94e18ddd9f206dc85d0c2362263f7fadd9
Calvin <Calvin.lacc@gmail.com>
2016-06-17 14:40:03 -0600

    finished remake of each

---
+++ each.rb
@@ -0,0 +1,13 @@
+# my version of the method each
+
+def each_remake (arr)
+   i = 0
+   while i < arr.size
+       yield(arr[i])
+       i+=1
+   end
+   arr
+end
+
+each_remake([1,2,3]) { |n| puts n**2 }
+
