7035c34642cab27eeea59ace746a60f924b43162
Calvin <Calvin.lacc@gmail.com>
2016-06-19 14:03:07 -0600

    WIP

--- passby.rb
+++ passby.rb
@@ -1,12 +1,24 @@
 def deepdupe (my_arr)
 
 	first_arr = my_arr.dup
+	i = 0
+	second_arr = []
+
+	while i > first_arr.size
+		first_arr.dup
+		second_arr.push first_arr.each
+	end 
 
 
-	nest = [first_arr]
-	print nest 	
+
+	nest = second_arr
+	print nest
+	print nest.object_id 
+	puts "hi"
+	puts nest[0].object_id	
+	puts my_arr[0].object_id
 
 
 end
 
-deepdupe ([[1,2,3],[5,6,7]])
+deepdupe ([[1,2,3],[5,6,7,8,9,9,9]])
